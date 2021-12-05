48a89d6df050e42d10c25b2df4b4413b5b67df71
Au3LT <a.stanaitis@ktu.edu>
2021-12-05 17:51:58 +0200

    Created Caesar Cipher program

---
+++ CaesarCipher/CaesarCipher.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarCipherProject
+{
+    public class CaesarCipher
+    {
+        static void Main(string[] args)
+        {
+            CaesarCipher p = new CaesarCipher();
+
+            string text = "ABCabcZz#!0139";
+            int lettersKey = 15;
+            int digitsKey = 4;
+
+            string encryptedText = p.GetEncrypedText(text, lettersKey, digitsKey);
+            string decryptedText = p.GetDecryptedText(encryptedText, lettersKey, digitsKey);
+
+            Console.WriteLine($"Your text: {text}");
+            Console.WriteLine($"Encryped text: {encryptedText}");
+            Console.WriteLine($"Decrypted text: {decryptedText}");
+        }
+
+        public string GetEncrypedText(string text, int lettersKey, int digitsKey)
+        {
+            return GetText(text, lettersKey, digitsKey);
+        }
+
+        public string GetDecryptedText(string encryptedText, int lettersKey, int digitsKey)
+        {
+
+            return GetText(encryptedText, 26 - lettersKey, 10 - digitsKey);
+        }
+
+        public Char getChangedLetter(char letter, int key)
+        {
+            char offset = Char.IsUpper(letter) ? 'A' : 'a';
+
+            return (char)((((letter + key) - offset) % 26) + offset);
+        }
+
+        public Char getChangedDigit(char digit, int key)
+        {
+            char offset = '0';
+
+            return (char)((((digit + key) - offset) % 10) + offset);
+        }
+
+        public string GetText(string text, int lettersKey, int digitsKey)
+        {
+            string newText = "";
+
+            foreach (char symbol in text)
+            {
+                switch (symbol)
+                {
+                    case char c when Char.IsLetter(c):
+                        newText += getChangedLetter(c, lettersKey);
+                        break;
+                    case char c when Char.IsDigit(c):
+                        newText += getChangedDigit(c, digitsKey);
+                        break;
+                    default:
+                        newText += symbol;
+                        break;
+                }
+            }
+            return newText;
+        }
+    }
+}
+
---
+++ CaesarCipherTesting/CaesarCipherUnitTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CaesarCipherProject;
+
+namespace CaesarCipherTesting
+{
+    [TestClass]
+    public class CaesarCipherUnitTests
+    {
+        [DataTestMethod]
+        [DataRow("ABCabcZz#!0139", 1, 1, "BCDbcdAa#!1240")]
+        [DataRow("ABCabcZz#!0139", 2, 3, "CDEcdeBb#!3462")]
+        [DataRow("ABCabcZz#!0139", 26, 10, "ABCabcZz#!0139")]
+        [DataRow("ABCabcZz#!0139", 15, 4, "PQRpqrOo#!4573")]
+        public void GetEncryptedTextTest(string text, int lettersKey, int digitsKey, string expected)
+        {
+            CaesarCipher caesarCipher = new CaesarCipher();
+            string encryptedText = caesarCipher.GetEncrypedText(text, lettersKey, digitsKey);
+            Assert.AreEqual(expected, encryptedText);
+        }
+
+        [DataTestMethod]
+        [DataRow("BCDbcdAa#!1240", 1, 1, "ABCabcZz#!0139")]
+        [DataRow("CDEcdeBb#!3462", 2, 3, "ABCabcZz#!0139")]
+        [DataRow("ABCabcZz#!0139", 26, 10, "ABCabcZz#!0139")]
+        [DataRow("PQRpqrOo#!4573", 15, 4, "ABCabcZz#!0139")]
+        public void GetDecryptedTextTest(string text, int lettersKey, int digitsKey, string expected)
+        {
+            CaesarCipher caesarCipher = new CaesarCipher();
+            string decryptedText = caesarCipher.GetDecryptedText(text, lettersKey, digitsKey);
+            Assert.AreEqual(expected, decryptedText);
+        }
+    }
+}
